988eef2fac9537babd550a01af3223da1dcbc455
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-02-06 17:48:16 -0300

    FEAT: REQ 4 DONE

--- src/middlewares/validateEmail.js
+++ src/middlewares/validateEmail.js
@@ -3,10 +3,14 @@ module.exports = (req, res, next) => {
     const { email } = req.body;
     console.log(email);
     const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-  
+    if (!email) {
+        return res.status(400).json(
+            { message: 'O campo "email" é obrigatório' },
+        );
+    }
     if (!emailRegex.test(email)) {
       return res.status(400).json(
-        { message: 'O campo Email é inválido!' },
+        { message: 'O "email" deve ter o formato "email@email.com"' },
       );
     }
   
--- src/middlewares/validatePassword.js
+++ src/middlewares/validatePassword.js
@@ -1,10 +1,14 @@
 // middlewares/validateCreatedAt.js
 module.exports = (req, res, next) => {
     const { password } = req.body; 
- 
+    if (!password) {
+        return res.status(400).json(
+          { message: 'O campo "password" é obrigatório' },
+        );
+      }
     if (password.length < 6) {
       return res.status(400).json(
-        { message: 'O campo password é inválido!' },
+        { message: 'O "password" deve ter pelo menos 6 caracteres' },
       );
     }
   
