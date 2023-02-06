37ec639fd19cc981938ec3e6145c7d5058da4d36
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-02-06 16:08:11 -0300

    FEAT: REQ 2 DONE

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -12,10 +12,20 @@ const dirPath = path.resolve(__dirname, '../talker.json');
     return JSON.parse(data);
 };
 
-talkerRouter.get('/', async (req, res) => {
+talkerRouter.get('/', async (_req, res) => {
    const data = await readFile(); 
    const talkerList = data; 
-   res.status(200).json(talkerList); 
+   return res.status(200).json(talkerList); 
 });
+
+talkerRouter.get('/:id', async (req, res) => {
+    const { params } = req;
+    const data = await readFile(); 
+    const talkerList = data.find(({ id }) => id === Number(params.id)); 
+    if (!talkerList) {
+        return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });   
+    }
+    return res.status(200).json(talkerList); 
+ });
 
 module.exports = talkerRouter;
